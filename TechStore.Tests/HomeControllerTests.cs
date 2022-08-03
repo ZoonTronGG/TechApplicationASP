50b8afa3472f7718926ea3c59231a7b48a7a2a72
Artyom Titorenko <vbymrf2@mail.ru>
2022-08-03 12:55:57 +0600

    Добавлены тесты и пагинация

--- TechStore.Tests/HomeControllerTests.cs
+++ TechStore.Tests/HomeControllerTests.cs
@@ -28,12 +28,40 @@ namespace TechStore.Tests
             IEnumerable<Product> result = (controller.Index() as ViewResult).ViewData.Model
                 as IEnumerable<Product>;
 
-            // Assert Утверждение
+            // Assert 
             Product[] prodArray = result.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P1", prodArray[0].Name);
             Assert.Equal("P2", prodArray[1].Name);
 
+        }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange Организация
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(mock => mock.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "Pr1"},
+                new Product {ProductID = 2, Name = "Pr2"},
+                new Product {ProductID = 3, Name = "Pr3"},
+                new Product {ProductID = 4, Name = "Pr4"},
+                new Product {ProductID = 5, Name = "Pr5"},
+            }).AsQueryable<Product>());
+
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act Поведение
+            IEnumerable<Product> result = (controller.Index(2) as ViewResult).ViewData.Model
+                as IEnumerable<Product>;
+
+            // Assert 
+            Product[] products = result.ToArray();
+            Assert.True(products.Length == 2);
+            Assert.Equal("Pr1", products[0].Name);
+            Assert.Equal("Pr2", products[1].Name);
         }
     }
 }
---
+++ TechStore/Infrastracture/PageLinkTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using TechStore.Models.ViewModels;
+
+namespace TechStore.Infrastracture
+{
+        [HtmlTargetElement("div", Attributes = "page-model")]
+        public class PageLinkTagHelper : TagHelper
+        {
+            private IUrlHelperFactory urlHelperFactory;
+
+            public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+            {
+                this.urlHelperFactory = urlHelperFactory;
+            }
+
+            [ViewContext]
+            [HtmlAttributeNotBound]
+            public ViewContext ViewContext { get; set; }
+            public PagingInfo PagingInfo { get; set; }
+            public string PageAction { get; set; }
+
+            public override void Process(TagHelperContext context, TagHelperOutput output)
+            {
+                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+                TagBuilder result = new TagBuilder("div");
+                for (int i = 1; i <= PagingInfo.TotalPages; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+                    tag.Attributes["href"] = urlHelper.Action(PageAction,
+                        new { productPage = i });
+                    tag.InnerHtml.Append(i.ToString());
+                    result.InnerHtml.AppendHtml(tag);
+                }
+                output.Content.AppendHtml(result.InnerHtml);
+            }
+        }
+}
