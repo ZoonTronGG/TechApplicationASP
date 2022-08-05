44906772419d42016bd02102a5d72eba1bf3eb09
Artyom Titorenko <vbymrf2@mail.ru>
2022-08-05 17:49:12 +0600

    Added bootstrap and products were styled on the web page

--- TechStore/Infrastracture/PageLinkTagHelper.cs
+++ TechStore/Infrastracture/PageLinkTagHelper.cs
@@ -22,8 +22,12 @@ namespace TechStore.Infrastracture
             public ViewContext ViewContext { get; set; }
             public PagingInfo PageModel { get; set; }
             public string PageAction { get; set; }
+            public bool PageClassesEnabled { get; set; } = false;
+            public string PageClass { get; set; }
+            public string PageClassNormal { get; set; }
+            public string PageClassSelected { get; set; }
 
-            public override void Process(TagHelperContext context, TagHelperOutput output)
+        public override void Process(TagHelperContext context, TagHelperOutput output)
             {
                 IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
                 TagBuilder result = new TagBuilder("div");
@@ -32,7 +36,15 @@ namespace TechStore.Infrastracture
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction,
                         new { productPage = i });
-                    tag.InnerHtml.Append(i.ToString());
+
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                    ? PageClassSelected : PageClassNormal);
+                }
+
+                tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
                 output.Content.AppendHtml(result.InnerHtml);
